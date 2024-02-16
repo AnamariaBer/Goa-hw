4f1caf3fa49199302a0a40d78a46b7efff4bcd58
AnamariaBer <anamariaber2008@gmail.com>
2024-02-16 20:17:11 +0400

    third commit

---
+++ day4/main.py
@@ -0,0 +1,8 @@
+my_name = "Anamaria"
+my_surname = "Beridze"
+my_age = 15
+future_age = my_age + 10
+
+print(f"My name is {my_name}. My surname is {my_surname}. "
+      f"I am {my_age} years old. In 10 years I will be {future_age} years old.")   
+
